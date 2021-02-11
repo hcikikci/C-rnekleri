d9a1fd652b1d963171e40540acc74ef0d78fbd63
Halitcan Çıkıkçı <halitcancikikci@gmail.com>
2021-02-11 23:50:05 +0300

    Add files via upload

---
+++ sayiyazdir.cpp
@@ -0,0 +1,9 @@
+// bir sayi girilmesini ve girilen sayiyi ekrana yazdýran program
+#include<stdio.h>
+int main(){
+	int sayi;
+	printf("Lutfen bir sayi giriniz :");
+	scanf("%d", &sayi);
+	printf("Girdiginiz sayi : %d",sayi);
+	return 0;
+}
